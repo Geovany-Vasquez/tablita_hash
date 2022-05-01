462da040686e60903eca8b1943336709bb6167e7
Geovany Vasquez <gvasquezp5@miumg.edu.gt>
2022-05-01 17:26:17 -0600

    tablitas

---
+++ src/tare_tablas_hash/Tabla_hash.java
@@ -0,0 +1,19 @@
+
+package tare_tablas_hash;
+/**
+ *
+ * @author Geovany Vasquez
+ */
+public class Tabla_hash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_tablashash tabla = new frm_tablashash();
+        tabla.show();
+        
+    }
+    
+}
